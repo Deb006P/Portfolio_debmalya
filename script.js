69d13b00bebee0d2f8ae99ec95b99cf34b9ac366
Debmalya Pan <134578586+Deb006P@users.noreply.github.com>
2024-02-17 22:11:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function redirect() {
+    window.location = "https://www.youtube.com/"
+}
+function redirect_2() {
+    window.location = "css certificate.pdf"
+  
+}
+function alert_1() {
+    window.alert ("Open GitHub")
+    
+}
+function alert_2() {
+    window.alert ("Download CV")
+    
+}
+function scroll() {
+    window.scrollTo(0, document.getElementsById("sec-2").offsetTop);
+}
